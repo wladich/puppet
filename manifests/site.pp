04e28fa826aab4c404659d09cf175f217ca6b6d3
root <root@tttt.(none)>
2013-12-09 04:14:52 +0400

    added postgres module

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 import 'users.pp'
 
 Exec { 
-      path        => '/usr/bin:/bin:/usr/sbin:/sbin',
+      path => '/usr/bin:/bin:/usr/sbin:/sbin',
 }
 
 class { "ssh::server":
@@ -14,4 +14,16 @@ class { 'sudo': }
 class { 'timezone':
     timezone => 'Europe/Moscow',
     autoupgrade => true
+}
+
+class { 'postgresql::server': 
+    pg_hba_conf_defaults => false
+}
+
+postgresql::server::pg_hba_rule { 'local access as postgres user':
+  type        => 'local',
+  database    => 'all',
+  user        => 'postgres',
+  auth_method => 'ident',
+  order       => '001',
 }
