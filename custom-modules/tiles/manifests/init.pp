300dcddf93402f02cbd85b3b38cc31cc1e909c18
Sergej Orlov <wladimirych@gmail.com>
2013-12-19 03:34:33 +0400

    nginx config with proxy for tiles server

--- custom-modules/tiles/manifests/init.pp
+++ custom-modules/tiles/manifests/init.pp
@@ -1,5 +1,4 @@
-class tiles {
-    include tiles::user
+class tiles ($minute_diff_url){
     include tiles::mappodm_mapnik_style
 
     class { 'apache':
@@ -10,5 +9,15 @@ class tiles {
     }    
 
     include tiles::mod_tile_config
+
+    class {'tiles::replicate_to_postgres':
+        minute_diff_url => $minute_diff_url
+    }
+
+    include nginx
+    file {'/etc/nginx/sites_enabled/tiles.conf':
+        source => 'puppet:///modules/tiles/nginx.config',
+        notify => Service['nginx']
+    }
 
 }
