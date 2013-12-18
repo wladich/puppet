a926d58440419c50f13b4050b3bc49d3a65dcf2a
Sergej Orlov <wladimirych@gmail.com>
2013-12-19 03:30:50 +0400

    classes to import and update data in pg

---
+++ custom-modules/tiles/manifests/replicate_to_postgres.pp
@@ -0,0 +1,66 @@
+class tiles::replicate_to_postgres ($minute_diff_url) {
+    include osmosis    
+    include osm2pgsql
+    include tiles::user_replicator
+
+    postgresql::server::database { "gis":
+        owner => "replicator",
+        encoding => "UTF-8",
+        locale =>  "en_US.UTF-8",
+        notify => Exec["setup postgis for db gis for user replicator"],
+        require => Class['tiles::user_replicator'],
+    }
+    
+    exec {"setup postgis for db gis for user replicator":
+        user => 'postgres',
+        refreshonly => true,
+        command => 'psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql &&\
+                    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql &&\
+                    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis_comments.sql &&\
+                    psql -d gis -c "GRANT SELECT ON spatial_ref_sys TO PUBLIC;"
+                    psql -d gis -c "GRANT ALL ON geometry_columns TO replicator;"
+                    '
+    }
+
+
+    file {'/var/lib/osm_replicate/configuration.txt':
+        content => "baseUrl=${minute_diff_url}\nmaxInterval = 3600",
+        notify => Service['replicated']
+    }
+
+    file {'/var/lib/osm_replicate/state.txt':
+        replace => false,
+        source => 'puppet:///modules/tiles/state.txt.bootstrap',
+        owner => 'replicator'
+    }
+
+    file {'/usr/local/bin/replicated':
+        source => 'puppet:///modules/tiles/replicated',
+        notify => Service['replicated'],
+        mode => 755
+    }
+
+    file {'/etc/init/replicated.conf':
+        source => 'puppet:///modules/tiles/init.replicated.conf',
+        notify => Service['replicated']
+    }
+
+    file {'/var/log/replicated':
+        ensure => directory,
+        owner => 'replicator'
+    }
+
+    file {'/var/lib/osm_replicate/osm2pgsql.style':
+        source => 'puppet:///modules/tiles/osm2pgsql.style',
+        notify => Service ['replicated']
+    }
+
+    service {'replicated':
+        ensure => running,
+        enable => true,
+        require => [File['/var/lib/osm_replicate/state.txt'],
+                    Exec["setup postgis for db gis for user replicator"],
+                    File['/var/log/replicated']]
+    }
+}
+
---
+++ custom-modules/tiles/manifests/user_replicator.pp
@@ -0,0 +1,21 @@
+class tiles::user_replicator {
+    user { 'replicator': 
+        shell => '/bin/false',
+        managehome => no,
+        gid => 'nogroup',
+    }
+
+    file {'/var/lib/osm_replicate':
+        ensure => directory,
+        owner => 'replicator'
+    }
+
+    postgresql::server::pg_hba_rule { 'access to db gis for user replicator':
+      type        => 'local',
+      database    => 'gis',
+      user        => 'replicator',
+      auth_method => 'ident',
+    }
+
+    postgresql::server::role { "replicator": }
+}
