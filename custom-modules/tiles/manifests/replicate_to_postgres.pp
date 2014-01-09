9c26ecf060fb4a3896f16aad7453ef7e1aae60bf
Sergej Orlov <wladimirych@gmail.com>
2014-01-10 00:48:28 +0400

    use postgis 2.0 for gis database

--- custom-modules/tiles/manifests/replicate_to_postgres.pp
+++ custom-modules/tiles/manifests/replicate_to_postgres.pp
@@ -7,24 +7,13 @@ class tiles::replicate_to_postgres ($minute_diff_url) {
         owner => "replicator",
         encoding => "UTF-8",
         locale =>  "en_US.UTF-8",
-        notify => Exec["setup postgis for db gis for user replicator"],
+        notify => Postgis2::Extension['gis'],
         require => Class['tiles::user_replicator'],
     }
-    
-    package {"postgresql-9.1-postgis":}
 
-    exec {"setup postgis for db gis for user replicator":
-        user => 'postgres',
-        refreshonly => true,
-        require => Package["postgresql-9.1-postgis"],
-        command => 'psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql &&\
-                    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql &&\
-                    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis_comments.sql &&\
-                    psql -d gis -c "GRANT SELECT ON spatial_ref_sys TO PUBLIC;"
-                    psql -d gis -c "GRANT ALL ON geometry_columns TO replicator;"
-                    '
+    postgis2::extension{'gis':
+        owner => "replicator"
     }
-
 
     file {'/var/lib/osm_replicate/configuration.txt':
         content => "baseUrl=${minute_diff_url}\nmaxInterval = 3600",
@@ -55,7 +44,7 @@ class tiles::replicate_to_postgres ($minute_diff_url) {
     service {'replicated':
         enable => true,
         require => [
-                    Exec["setup postgis for db gis for user replicator"],
+                    Postgis2::Extension['gis'],
                     File['/var/log/replicated']]
     }
 }
